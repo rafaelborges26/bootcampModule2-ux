55a3e5db46098a33595aa80e16537695144f121d
= <rafael.borges2698@gmail.com>
2022-01-21 19:09:40 -0300

    added build for to test deploy

---
+++ build/precache-manifest.d8a4e8e6a95d312fa6980a55529b68cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b468ab6cc3929fec8cb2479ff8cdfd",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "5b9670b739f6b03676b3",
+    "url": "/react-app/static/css/main.f42e757e.chunk.css"
+  },
+  {
+    "revision": "10ae0603604359519bfb",
+    "url": "/react-app/static/js/2.50c414a1.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-app/static/js/2.50c414a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b9670b739f6b03676b3",
+    "url": "/react-app/static/js/main.3999f2c1.chunk.js"
+  },
+  {
+    "revision": "a0869fd3ed4d6a5b8e7c",
+    "url": "/react-app/static/js/runtime-main.d74d1e89.js"
+  }
+]);
